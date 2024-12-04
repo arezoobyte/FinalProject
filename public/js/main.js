d7bd90fddbedb40a6e51ae61d64fda42bc7f3d17
arezoobyte <tinagio30@gmail.com>
2024-12-05 01:29:39 +0330

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,3 @@
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../css/Main.css";
+import "../css/main.css";
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
